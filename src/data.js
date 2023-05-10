acb199c5828d2ce89d7ec8e869d2e8ca50692c73
Ricardo Camillo Avakian <rcavakian@MacBook-Air-de-Ricardo.local>
2023-05-09 19:00:08 -0600

    Primeiro Commit

---
+++ src/data.js
@@ -0,0 +1,64 @@
+const data = {
+    banner: {img: require( '.images/')},
+    moodWeekend: {
+        title: 'Mood Weekend',
+        rightIcon: require('../../../../../'),
+        images: [
+            {
+                img: require('.images/modoWweekend'),
+            },
+            {
+                img: require('.images/modoWweekend/'),
+            },
+            {
+                img: require('.images/modoWweekend/'),
+            },
+            {
+                img: require('.images/modoWweekend/'),
+            },
+            {
+                img: require('.images/modoWweekend/'),
+            },
+        ]
+    },
+    topAlbums: {
+        title: 'Top Albums',
+        images: [
+            {
+                img: require('.images/topAlbums/'),
+            },
+            {
+                img: require('.images/topAlbums/'),
+            },
+            {
+                img: require('.images/topAlbums/'),
+            },
+            {
+                img: require('.images/topAlbums/'),
+            },
+            {
+                img: require('.images/topAlbums/'),
+            },
+        ] 
+    },
+    browseAll: {
+        title: 'Browse All',
+        images: [
+            {
+                img: require('.images/browseAll/'),
+            },
+            {
+                img: require('.images/browseAll/'),
+            },
+            {
+                img: require('.images/browseAll/'),
+            },
+            {
+                img: require('.images/browseAll/'),
+            },
+            {
+                img: require('.images/browseAll/'),
+            },
+        ]
+    }
+};
