bb53ef2ab99c2e370809e5d7ecb30c124ca0ae5b
Ricardo Avakian <rcavakian@gmail.com>
2023-05-16 21:48:53 -0600

    Add images path

--- src/data.js
+++ src/data.js
@@ -1,23 +1,23 @@
 const data = {
-    banner: {img: require( '.images/')},
+    banner: {img: require( './images/banner.jpeg')},
     moodWeekend: {
         title: 'Mood Weekend',
-        rightIcon: require('../../../../../'),
+        rightIcon: require('./images/right_icon.png'),
         images: [
             {
-                img: require('.images/modoWweekend'),
+                img: require('./images/moodWeekend/caipirinha.jpeg'),
             },
             {
-                img: require('.images/modoWweekend/'),
+                img: require('./images/moodWeekend/biscoito_globo.jpeg'),
             },
             {
-                img: require('.images/modoWweekend/'),
+                img: require('./images/moodWeekend/boipeba.jpeg'),
             },
             {
-                img: require('.images/modoWweekend/'),
+                img: require('./images/moodWeekend/farol_itapua.jpeg'),
             },
             {
-                img: require('.images/modoWweekend/'),
+                img: require('./images/moodWeekend/porto_da_barra.jpeg'),
             },
         ]
     },
@@ -25,19 +25,19 @@ const data = {
         title: 'Top Albums',
         images: [
             {
-                img: require('.images/topAlbums/'),
+                img: require('./images/topAlbums/madonnaErotica.jpg'),
             },
             {
-                img: require('.images/topAlbums/'),
+                img: require('./images/topAlbums/cazwell.jpg'),
             },
             {
-                img: require('.images/topAlbums/'),
+                img: require('./images/topAlbums/montero.jpg'),
             },
             {
-                img: require('.images/topAlbums/'),
+                img: require('./images/topAlbums/faith.jpg'),
             },
             {
-                img: require('.images/topAlbums/'),
+                img: require('./images/topAlbums/lost_highway.jpg'),
             },
         ] 
     },
@@ -45,20 +45,35 @@ const data = {
         title: 'Browse All',
         images: [
             {
-                img: require('.images/browseAll/'),
+                img: require('./images/browseAll/daily_mix_1.jpeg'),
             },
             {
-                img: require('.images/browseAll/'),
+                img: require('./images/browseAll/disco_hi_life.jpg'),
             },
             {
-                img: require('.images/browseAll/'),
+                img: require('./images/browseAll/the_roller_rink.jpeg'),
             },
             {
-                img: require('.images/browseAll/'),
+                img: require('./images/browseAll/this_is_diana_ross.jpeg'),
             },
             {
-                img: require('.images/browseAll/'),
+                img: require('./images/browseAll/this_is_donna_summer.jpeg'),
             },
+            {
+                img: require('./images/browseAll/this_is_kraftwerkjpeg.jpeg'),
+            },
+            {
+                img: require('./images/browseAll/this_is_lilnasx.jpeg'),
+            },
+            {
+                img: require('./images/browseAll/this_is_madonna.jpeg'),
+            },
+            {
+                img: require('./images/browseAll/this_is_rita_lee.jpeg'),
+            },
+            {
+                img: require('./images/browseAll/this_is_sister_sledge.jpg'),
+            }
         ]
     }
 };
